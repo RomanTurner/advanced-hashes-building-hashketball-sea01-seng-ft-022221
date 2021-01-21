56c54eb0800834f09a34c2cd2378947ecb279319
Roman Turner <rn.turner@live.com>
2021-01-21 19:21:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 # Write your code here!
 require 'pry'
 def game_hash 
-  hashketball{
+{
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
@@ -29,26 +29,26 @@ def game_hash
           slam_dunks: 7 
         },
         { 
-          :player_name => "Brook Lopez",
-          :number => 11,
-          :shoe => 17,
-          :points => 17, 
-          :rebounds => 19,
-          :assists => 10,
-          :steals => 3, 
-          :blocks => 1, 
-          :slam_dunks => 15 
+          player_name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17, 
+          rebounds: 19,
+          assists: 10,
+          steals: 3, 
+          blocks: 1, 
+          slam_dunks: 15 
         },
         { 
-          :player_name => "Mason Plumlee",
-          :number => 1,
-          :shoe => 19,
-          :points => 26, 
-          :rebounds => 11,
-          :assists => 6,
-          :steals => 3,
-          :blocks => 8, 
-          :slam_dunks => 5 
+          player_name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26, 
+          rebounds: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8, 
+          slam_dunks: 5 
         },
         
           
