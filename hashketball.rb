bfecfd130cb35c75e4910447a9359127a7c9db83
Roman Turner <rn.turner@live.com>
2021-01-21 19:08:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,9 +5,9 @@ def game_hash
     :home = {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: {
-        
-      }
+      players: [
+        {}
+        ]
     },
     :away = {}
   }
