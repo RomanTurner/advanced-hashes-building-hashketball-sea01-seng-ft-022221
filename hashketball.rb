2acd8049c12c6ead8275aa91259bb9d8ca42b591
Roman Turner <rn.turner@live.com>
2021-01-21 19:15:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
           :assists => 12, 
           :steals => 3, 
           :blocks => 1,
-          :slam_dunks => 1 
+          :slam_dunks => 1
         },
         { 
           :player_name: "Reggie Evans", 
@@ -26,7 +26,20 @@ def game_hash
           :assists => 12, 
           :steals => 12, 
           :blocks => 12, 
-          :slam_dunks => 7 },
+          :slam_dunks => 7 
+        },
+        { 
+          :player_name => "Brook Lopez",
+          :number => 11,
+          :shoe => 17,
+          :points => 17, 
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3, 
+          :blocks => 1, 
+          :slam_dunks => 15 
+        },
+        
           
         ]
     },
